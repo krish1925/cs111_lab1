ebb496e2c5d96094c5350be28c4459df2e977f50
Krish Patel <krishhnpatel@gmail.com>
2023-10-23 17:12:27 -0700

    pipe fix

--- README.md
+++ README.md
@@ -5,13 +5,15 @@ For this lab we create a program that runs command line arguments and replicates
 
 ## Building
 
-To build this program, we run make on the virtual machines that we set up
-Explain briefly how to build your program
+To build this program, you need to execute the makefile in the directory by running it in terminal. 
+Open terminal, navigate to the directory where the files are located, and then run the command: make
+this would run the makefile with the specific Cflags and FDflags, creating another executable file pipe.
 
 ## Running
-
-Show an example run of your program, using at least two additional arguments, and what to expect
-
+to run this executable file pipe, we write the command ./pipe [command1] [command2] .....[command n], and run this in the terminal. if there are any errors with the piping, or syntax errors, it would send errors and would write that to the terminal.
+for instance, the command : 
+./pipe ls sort 
+will list all of the files and folders in the current directory in a sorted manner
 ## Cleaning up
 
 Explain briefly how to clean up all binary files
--- pipe.c
+++ pipe.c
@@ -4,39 +4,44 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char * argv[]) {
-    if(argc < 2) {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
         printf("Invalid number of arguments\n");
-        exit(EXIT_FAILURE); // Use EXIT_FAILURE for generic failure exit
+        exit(EXIT_FAILURE);
     }
 
     int pipes[argc - 1][2];
 
-    for(int i = 0; i < argc - 1; i++) {
-        if(pipe(pipes[i]) == -1) {
-            perror("Invalid pipe");
+    for (int i = 0; i < argc - 1; i++) {
+        if (pipe(pipes[i]) == -1) {
+            perror("Pipe creation failed");
             exit(EXIT_FAILURE);
         }
 
         pid_t pid = fork();
-        if(pid == -1) {
+        if (pid == -1) {
             perror("Fork failed");
             exit(EXIT_FAILURE);
-        } else if(pid == 0) { // This is the child process.
-            if(i != 0) {
-                close(pipes[i-1][1]);
-                dup2(pipes[i-1][0], STDIN_FILENO);
+        } else if (pid == 0) { // Child process
+            if (i != 0) { // Not the first command
+                dup2(pipes[i - 1][0], STDIN_FILENO);
+                close(pipes[i - 1][0]);
+                close(pipes[i - 1][1]);
             }
-            close(pipes[i][0]);
+
             dup2(pipes[i][1], STDOUT_FILENO);
+            close(pipes[i][0]);
             close(pipes[i][1]);
 
-            char *args[] = {argv[i+1], NULL};
+            char *args[] = {argv[i + 1], NULL};
             execvp(args[0], args);
             perror("Exec Error");
             exit(EXIT_FAILURE);
-        } else {
+        } else { // Parent process
             close(pipes[i][1]);
+            if (i != 0) {
+                close(pipes[i - 1][0]);
+            }
         }
     }
 
@@ -48,19 +53,18 @@ int main(int argc, char * argv[]) {
         if (argc > 2) {
             dup2(pipes[argc - 2][0], STDIN_FILENO);
             close(pipes[argc - 2][0]);
+            close(pipes[argc - 2][1]);
         }
         char *args[] = {argv[argc - 1], NULL};
         execvp(args[0], args);
         perror("Exec error");
         exit(EXIT_FAILURE);
     } else {
-        if(argc > 2) {
-            close(pipes[argc - 2][0]);
-        }
+        close(pipes[argc - 2][0]);
     }
 
-    // Wait for all child processes to finish before the parent exits.
-    for(int i = 1; i < argc; i++) {
+    // Wait for all child processes to finish.
+    for (int i = 0; i < argc - 1; i++) {
         wait(NULL);
     }
 
