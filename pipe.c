05cafbd381c3fc8483f6a9c8923349c9d284cc72
Krish Patel <krishhnpatel@gmail.com>
2023-10-24 23:45:59 -0700

    Final commit

--- pipe.c
+++ pipe.c
@@ -8,61 +8,47 @@
 int main(int argc, char *argv[]) {
     if (argc < 2) { // at least one command is required
         fprintf(stderr, "Invalid number of arguments\n");
-        exit(EINVAL);
+        exit(EINVAL);  // exit with einval
     }
-
     int pipes[argc - 2][2];
-    pid_t pids[argc - 1];
-
+    pid_t pids[argc - 1]; //keeping track of all pids
     for (int i = 0; i < argc - 2; i++) {
         if (pipe(pipes[i]) == -1) {
             perror("Pipe creation failed");
             exit(-1);
-        }
-    }
-
-    for (int i = 0; i < argc - 1; i++) {
+        }}
+    for(int i = 0; i < argc - 1; i++) {
         pid_t pid = fork();
         if (pid == -1) {
             perror("Fork failed");
-            exit(-1);
+            exit(-2);
         } else if (pid == 0) { // Child process
             if (i != 0) { // Not the first command
                 dup2(pipes[i - 1][0], STDIN_FILENO);
             }
-            
-            if (i != argc - 2) { // Not the last command
-                dup2(pipes[i][1], STDOUT_FILENO);
-            }
-            
+            if (i != argc - 2) {
+                dup2(pipes[i][1], STDOUT_FILENO);}
             for (int j = 0; j < argc - 2; j++) {
                 close(pipes[j][0]);
                 close(pipes[j][1]);
             }
-
             char *args[] = {argv[i + 1], NULL};
             execvp(args[0], args);
             perror("Exec Error");
-            exit(-1);
+            exit(-3);
         } else {
             pids[i] = pid;
-        }
-    }
-
-    // Close all pipe ends in the parent
+        }}
     for (int i = 0; i < argc - 2; i++) {
         close(pipes[i][0]);
         close(pipes[i][1]);
     }
-
     int exit_code = 0;
-    for (int i = 0; i < argc - 1; i++) {
+    for (int i = 0; i <argc-1; i++) {
         int status;
         waitpid(pids[i], &status, 0);
         if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
             exit_code = WEXITSTATUS(status);
-        }
-    }
-
-    exit(exit_code);
+        }}
+    exit(exit_code); // returns the exit code (0 if successful)
 }
